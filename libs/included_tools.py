4c5bb41289a19340847b7056146c29f4a775eed7
James Hayward <jamesjhayward99@gmail.com>
2023-04-14 12:42:24 +0100

    Initial

---
+++ libs/included_tools.py
@@ -0,0 +1,68 @@
+# This folder is used to hold the code that's inherent to the project
+
+# IMPORTS
+import configparser                                                                 # For the config function
+import os                                                                           # Checking existence of files
+import sys                                                                          # For exiting the program if the checks fail
+
+# CODE
+def run_file_check(check_file_name:str) -> None:                                    # Checks the list of files to ensure all are there
+    # INITIALISE
+    flag_missing_files = False                                                      #   # Flag that triggers if there's a missing file
+    list_missing_files = []                                                         #   # Archives a list of missing files
+
+    # OPEN FILE
+    with open(check_file_name, "r") as file:
+        # LINES TO LIST
+        file_lines = file.readlines()
+
+        # CHECK LINES
+        for line in file_lines:
+            # REMOVE NEWLINE SPECIAL CHARACTER
+            line = line.strip("\n")
+            
+            # CONDITIONS
+            # If the Line is Empty
+            if line == "":
+                continue
+
+            # If the Line is a Comment
+            if line.strip().startswith("#"):                                        #   # Removes whitespace then checks if the first is #
+                continue
+            
+            # GET EXISTENCE
+            existence = os.path.exists(line)                                        #   # Checks the existence of the file
+
+            # FLAG TRIGGER OF MISSING FILES
+            if not existence:                                                       #   # If the files are missing
+                flag_missing_files = True
+                list_missing_files.append(line)
+
+            
+            # PRINT THE EXISTANCE OF FILE
+            if get_config_value("Included", "show_checks") == "True":
+                print(f"CHCK: Existence of \"{line}\" -> {existence}")
+        
+        if flag_missing_files:
+            # STATE ISSUE
+            print("CHCK: Failed Check -> Missing Files")
+            
+            # LIST FILE
+            for file in list_missing_files:
+                print(f"CHCK: Missing File -> \"{file}\"")
+            
+            # SHUTS DOWN PROGRAM
+            sys.exit("CHCK: Stopping Program")
+        
+        print("CHCK: Checks Passed")
+
+
+
+
+def get_config_value(section:str, value:str) -> str:                                # Streamlines the config value acquisition
+    # CONFIG INITIATION
+    config = configparser.ConfigParser()                                            #   # Initiates Parser
+    config.read("config.ini")                                                       #   # Hardcoded config.ini file
+
+    # RETURN
+    return config[section][value].strip("\"")                                       #   # Removes un-needed ""
---
+++ main.py
@@ -0,0 +1,90 @@
+# LEGAL AND DOCUMENTATION
+# Copyright (c) 2023 James Hayward
+
+
+# IMPORTS
+# Official Modules
+
+
+# Personal Modules
+import libs.included_tools as inc_tools
+
+# CONSTANTS
+# Included
+PROJECT_NAME        = inc_tools.get_config_value("Metadata", "project_name")        # What is the title of the program
+PROJECT_AUTHOR      = inc_tools.get_config_value("Metadata", "project_author")      # Who made the file
+PROJECT_YEAR        = inc_tools.get_config_value("Metadata", "project_year")        # What is the current year
+
+PROJECT_VERSION     = inc_tools.get_config_value("Metadata", "project_version")     # What is the current version
+
+# Program
+
+# LOOK-UP TABLES
+
+
+# GLOBAL VARIABLES
+
+
+# MAIN PIPELINE
+def main_pipeline():                                                                # This is the main pipeline of what the code runs
+    # INITIATE PROGRAM
+    print(f"=== {PROJECT_NAME.upper()} ===")                                        #   # Program name
+    print(f"Written by {PROJECT_AUTHOR}, {PROJECT_YEAR}")                           #   # Program details
+    print()
+    print(f"BOOT: Version -> {PROJECT_VERSION}")                                    #   # Program version
+
+    # CHECK PIPELINE
+    check_pipeline()                                                                #   # This is used to check, leave as pass if not needed
+
+    # PROGRAM START
+    #
+
+    # SHUT DOWN
+
+
+
+def check_pipeline():                                                               # This is the pipeline that runs checks if required
+    # INITIALISE
+    print("CHCK: Running Checks")
+
+    # CHECK FILES
+    inc_tools.run_file_check(".checks")                                             #   # Ensures all files and folders are present
+
+
+# CLASSES
+
+
+# FUNCTIONS
+# Pipeline Functions
+
+
+# Utility Functions
+
+
+# MAIN
+if __name__ == '__main__':
+   main_pipeline()                                                                  # Runs the main pipeline
+
+
+# DESCRIPTION
+"""
+    _summary_
+
+
+"""
+
+
+# NOTES
+"""
+    _notes_
+
+"""
+
+
+# PLAN
+"""
+    _plan_
+
+"""
+
+
