b196ddd11e1d0b324fee62397c2c6ed6bf371863
James Hayward <jamesjhayward99@gmail.com>
2023-05-02 14:36:06 +0100

    Included some bits

--- main.py
+++ main.py
@@ -2,26 +2,51 @@
 # Copyright (c) 2023 James Hayward
 
 
+# DESCRIPTION
+"""
+    _summary_
+
+
+"""
+
+
+# NOTES
+"""
+    _notes_
+
+"""
+
+
 # IMPORTS
 # Official Modules
 
 
 # Personal Modules
-import libs.included_tools as inc_tools
+import libs.included_tools as included
 
 
 # CONSTANTS
 # Included
-PROJECT_NAME        = inc_tools.get_config_value("Metadata", "project_name")        # What is the title of the program
-PROJECT_AUTHOR      = inc_tools.get_config_value("Metadata", "project_author")      # Who made the file
-PROJECT_YEAR        = inc_tools.get_config_value("Metadata", "project_year")        # What is the current year
+PROJECT_NAME        = included.get_config_value("Metadata", "project_name")         # What is the title of the program
+PROJECT_AUTHOR      = included.get_config_value("Metadata", "project_author")       # Who made the file
+PROJECT_YEAR        = included.get_config_value("Metadata", "project_year")         # What is the current year
 
-PROJECT_VERSION     = inc_tools.get_config_value("Metadata", "project_version")     # What is the current version
+PROJECT_VERSION     = included.get_config_value("Metadata", "project_version")      # What is the current version
 
-# Program
+# Paths
+
+
+# Values
 
 
 # LOOK-UP TABLES
+
+
+# HASHMAPS
+# Initiation
+
+
+# Hard-coded Hashmaps
 
 
 # GLOBAL VARIABLES
@@ -35,14 +60,17 @@ def main_pipeline():                                                            
     print()
     print(f"BOOT: Version -> {PROJECT_VERSION}")                                    #   # Program version
 
+    
     # CHECK PIPELINE
     check_pipeline()                                                                #   # This is used to check, leave as pass if not needed
 
-    # PROGRAM START
+    
+    # PROGRAM PIPELINE
+    program_pipeline()
     
 
-    # SHUT DOWN
-
+    # SCRIPT SHUTDOWN
+    print("MAIN: Program Finished")
 
 
 def check_pipeline():                                                               # This is the pipeline that runs checks if required
@@ -50,7 +78,20 @@ def check_pipeline():                                                           
     print("CHCK: Running Checks")
 
     # CHECK FILES
-    inc_tools.run_file_check(".checks")                                             #   # Ensures all files and folders are present
+    included.run_file_check(".checks")                                              #   # Ensures all files and folders are present
+
+
+def program_pipeline():                                                             # Runs the program proper
+    # INITIATION
+    # Prompt
+    print("MAIN: Running Program")
+
+
+    # PROGRAM PROPER
+
+
+    # PROGRAM SHUTDOWN
+
 
 
 # CLASSES
@@ -67,20 +108,6 @@ def check_pipeline():                                                           
 if __name__ == '__main__':
    main_pipeline()                                                                  # Runs the main pipeline
 
-
-# DESCRIPTION
-"""
-    _summary_
-
-
-"""
-
-
-# NOTES
-"""
-    _notes_
-
-"""
 
 
 # PLAN
